2c076aa69cfc8b3e9c3352b3e0334619ff0489e1
mollikacomputer <mollikacomputer3@gmail.com>
2023-01-28 16:50:07 +0600

    payload

--- src/Pages/Parent.js
+++ src/Pages/Parent.js
@@ -1,35 +1,28 @@
 import React, {useReducer } from "react";
-
 const Parent = () => {
   const initialState = 0;
   const reducer = (state, action) =>{
     if(action.type === "INCREMENT"){
-      return state + 1;
+      return state + action.payload;
     } else if(action.type === "DECREMENT"){
-      return state - 1;
+      return state - action.payload;
     }
   };
   const [state, dispatch] = useReducer(reducer, initialState);
-
   return (
     <div className="grid justify-items-center ">
         <h2 className="text-2xl my-6">Parent {state} </h2>
           <div>
             <button
               className="btn btn-warning mr-2"
-              onClick={() => dispatch({type:"DECREMENT"})}
-            >
-              Decrise
-            </button>
+              onClick={() => dispatch({type:"DECREMENT", payload:5})}
+            >Decrise</button>
             <button
               className="btn btn-error"
-              onClick={() => dispatch({type: "INCREMENT"})}
-            >
-              Increase
-            </button>
+              onClick={() => dispatch({type: "INCREMENT", payload:5 })}
+            > Increase</button>
           </div>
     </div>
   );
 };
-
 export default Parent;
